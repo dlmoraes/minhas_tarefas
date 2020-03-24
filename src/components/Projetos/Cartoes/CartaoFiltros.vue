9387cf7c851f6250b10b0c72af114f17959bed63
Diego Moraes <diegofrost96@gmail.com>
2020-03-24 20:54:31 -0300

    Ajustando cartão de filtros

---
+++ src/components/Projetos/Cartoes/CartaoFiltros.vue
@@ -0,0 +1,128 @@
+<template>
+  <q-card class="cartao_filtro shadow-1">
+    <q-markup-table class="tb_filtros" flat :v-ripple="false">
+      <tbody>
+      <tr>
+        <td>
+          <q-icon name="img:statics/icons/filter.png" size="36px" class="icone_titulo"/>
+          <span class="titulo q-ml-md">Filtros:</span>
+          <q-btn-dropdown class="q-ml-md" icon="img:statics/icons/timer.png" flat label="Criado Em" no-caps>
+            <q-list>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Limpar Filtro</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator/>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Hoje</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Está semana</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Este Mês</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Este Ano</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+          <q-btn-dropdown class="q-ml-md" icon="img:statics/icons/priority.png" flat label="Prioridade" no-caps>
+            <q-list>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Limpar Filtro</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator/>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Alta</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Média</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Baixa</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+          <q-btn-dropdown class="q-ml-md" icon="img:statics/icons/correct.png" flat label="Situação" no-caps>
+            <q-list>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Limpar Filtro</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator/>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Em Andamento</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Finalizada</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Aguardando</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </td>
+      </tr>
+      </tbody>
+    </q-markup-table>
+  </q-card>
+</template>
+
+<script>
+  export default {
+    name: "CartaoFiltros"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .cartao_filtro {
+    width: 100%;
+    height: 50px;
+
+    .filtros {
+      margin-top: -10px;
+    }
+
+    .tb_filtros {
+      .q-table tbody td:before {
+        background: transparent;
+      }
+    }
+  }
+</style>
--- src/pages/Projetos.vue
+++ src/pages/Projetos.vue
@@ -11,112 +11,8 @@
       <CartaoResumoIndividual descricao="Finalizado" :valor="5" cor="bg-positive" class="col-md-2 q-ma-sm"
                               icone="img:statics/icons/checklist_1.png"/>
     </div>
-    <div class="row">
-      <q-card class="cartao_filtro shadow-1">
-        <q-card-section class="row">
-          <div class="col-md-1 col-sm-3 cabecalho">
-            <q-icon name="img:statics/icons/filter.png" size="36px" class="icone_titulo"/>
-            <span class="titulo">Filtros:</span>
-          </div>
-          <div class="col-md-1 col-sm-3 botao_filtros">
-            <q-btn-dropdown icon="img:statics/icons/timer.png" flat label="Criado Em" no-caps>
-              <q-list>
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Limpar Filtro</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-separator/>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Hoje</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Está semana</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Este Mês</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Este Ano</q-item-label>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-btn-dropdown>
-          </div>
-          <div class="col-md-1 col-sm-3 botao_filtros">
-            <q-btn-dropdown icon="img:statics/icons/priority.png" flat label="Prioridade" no-caps>
-              <q-list>
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Limpar Filtro</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-separator/>
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Alta</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Média</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Baixa</q-item-label>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-btn-dropdown>
-          </div>
-          <div class="col-md-1 col-sm-3 botao_filtros">
-            <q-btn-dropdown icon="img:statics/icons/correct.png" flat label="Situação" no-caps>
-              <q-list>
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Limpar Filtro</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-separator/>
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Em Andamento</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Finalizada</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-                <q-item clickable v-close-popup>
-                  <q-item-section>
-                    <q-item-label>Aguardando</q-item-label>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-btn-dropdown>
-          </div>
-        </q-card-section>
-      </q-card>
+    <div class="row q-mb-md">
+      <CartaoFiltros />
     </div>
     <div class="row q-mb-md time">
       <div class="col-12">
@@ -168,16 +64,16 @@
 
 <script>
   import CartaoGrade from "components/Projetos/Cartoes/CartaoGrade";
-  import AreaResumo from "../components/Projetos/Tools/AreaResumo";
   import CartaoResumoIndividual from "../components/Projetos/Cartoes/CartaoResumoIndividual";
+  import CartaoFiltros from "../components/Projetos/Cartoes/CartaoFiltros";
 
 
   export default {
     name: "Projetos",
     components: {
       CartaoGrade,
-      CartaoResumoIndividual
-      // AreaResumo
+      CartaoResumoIndividual,
+      CartaoFiltros
     },
     data() {
       return {
@@ -218,32 +114,6 @@
 </script>
 
 <style lang="scss">
-  .cartao_filtro {
-    width: 100%;
-    height: 50px;
-    margin-top: 20px;
-    margin-bottom: 30px;
-
-    .cabecalho {
-      .titulo {
-        margin-top: -20px;
-        margin-left: 10px;
-      }
-
-      .icone_titulo.q-icon {
-        margin-top: -10px;
-      }
-    }
-
-    .botao_filtros {
-      margin-right: 25px;
-
-      .q-btn-dropdown {
-        margin-top: -10px;
-      }
-    }
-  }
-
   .time {
     .titulo {
       font-size: 16px;
